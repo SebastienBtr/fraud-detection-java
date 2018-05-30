52ff49103913901708f7c840acb329cfbe04453c
Le-Leah <happyvivus@gmail.com>
2018-05-30 15:15:29 +0200

    comp method fini

--- src/test/java/comparator/MethodComparatorTest.java
+++ src/test/java/comparator/MethodComparatorTest.java
@@ -18,16 +18,16 @@ public class MethodComparatorTest
 
     @Test
     public void testSameMethods(){
-        DefaultMutableTreeNode  method1 = tree.getNextNode();
-        DefaultMutableTreeNode method2 = tree.getNextNode();
-      int grade =  MethodComparator.compare(method1,method2);
+        DefaultMutableTreeNode  method1 = tree.getNextNode().getNextNode();
+
+      int grade =  MethodComparator.compare(method1,method1);
         //System.out.println(method1.toString()+  "\n"+method2.toString());
         System.out.println(grade);
     }
 
     @Test
     public void testDifferentMethods(){
-        DefaultMutableTreeNode  method1 = tree.getNextNode();
+        DefaultMutableTreeNode  method1 = tree.getNextNode().getNextNode();
         DefaultMutableTreeNode method2 = method1.getNextSibling();
         int grade =  MethodComparator.compare(method1,method2);
         //System.out.println(method1.toString()+  "\n"+method2.toString());
