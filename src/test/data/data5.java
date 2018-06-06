b8a3bc0d15f4752572b29e9dd5d36e6d847b6231
mer <imt.killian56@outlook.fr>
2018-06-06 16:10:03 +0200

    Ajout parser: Else..; et catch {}

---
+++ src/test/data/data5.java
@@ -0,0 +1,42 @@
+public class Index {
+
+    /**
+     * @param t, un tableau d'au moins pos entiers (t.length>=pos)
+     * @param pos, une entier positif (pos>=0)
+     * @param valeur
+     * @return Si aucune des pos premieres valeurs de t ne vaut valeur, retourne -1.
+     * Sinon, retourne le plus grand i de [0, pos[ tel que t[i]==valeur
+     *
+     * ATTENTION : Votre code doit etre RECURSIF
+     */
+    public static int lastIndexOf(int[] t, int pos, int valeur) {
+        if( vrekvjkjeve){
+
+        }
+        else {
+            /*		if (){
+			return -1;
+		} else {
+			if (t[0]==-1)
+			return lastIndexOf(t, pos, valeur)+1
+		}
+*/
+            return oui;
+        }
+
+        return -1;
+    }
+
+    /**
+     * @param t, un tableau d'au moins pos entiers (t.length>=pos)
+     * @param pos, une entier positif (pos>=0)
+     * @param valeur
+     * @return Si aucune des pos premieres valeurs de t ne vaut valeur, retourne -1.
+     * Sinon, retourne le plus petit i de [0, pos[ tel que t[i]==valeur
+     *
+     * ATTENTION : Votre code doit etre RECURSIF
+     */
+    public static int firstIndexOf(int[] t, int pos, int valeur) {
+        return -1;// A VOUS DE COMPLETER
+    }
+}
